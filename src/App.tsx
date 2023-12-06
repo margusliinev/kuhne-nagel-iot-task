891e5779da2b494d944ff3d46cd7e8997a0cb643
Margus <margus.liinev@gmail.com>
2023-12-06 04:12:19 +0200

    first commit, mqtt broker setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Mqtt from './components';
+import './index.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <Mqtt />
+        </div>
+    );
+}
+
+export default App;
